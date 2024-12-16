60dbb3437012473b1ea680cea471c73ea477d803
Ramiro Martinez <ramiro.nicolas.martinez14@gmail.com>
2024-12-16 18:49:28 -0300

    se agrega nueva cuenta a la db y se actualizan las vistas

--- src/components/Cuentas.jsx
+++ src/components/Cuentas.jsx
@@ -1,18 +1,18 @@
 import React, {useEffect, useState} from 'react';
 import '../css/cuentaCss.css'
-import '../controllers/cuentaController.js'
 import { getCuenta } from '../controllers/cuentaController.js';
 
-export const Cuentas = ({idUser,setIdAccounts}) => {
+
+export const Cuentas = ({idUser,setIdAccounts,refresh}) => {
     const [cuentas, setCuentas] = useState([]);
     
     useEffect(()=>{
         getCuenta(idUser,setCuentas);
-    }, []);
+    }, [refresh]);
     useEffect(() => {
         const ids = cuentas.map((cuenta) => cuenta.idAccount);
         setIdAccounts(ids);
-    }, [cuentas]);
+    }, [cuentas,refresh]);
 
   return ( 
         <div className='container-fluid p-0'>
@@ -23,12 +23,12 @@ export const Cuentas = ({idUser,setIdAccounts}) => {
                         <a href="#" className='container-fluid bg-transparent text-light'>
                             <div className='container-fluid card-body d-lg-flex justify-content-between align-items-center'
                             width="100%">
-                                <div>
+                                <div className='m-0'>
                                     <h6 className='card-title mb-0 '>{cuenta.description}</h6>
                                     <small className='text-light'>{cuenta.noAccount}</small>
                                 </div>
-                                <div className = "d-flex justify-content-start justify-content-lg-end"> 
-                                    <h5 className='mb-0'>{cuenta.description=='Caja de Ahorros en U$D' ? 'U$D ' : '$ '}{cuenta.balance}</h5>
+                                <div className = "d-flex justify-content-start justify-content-lg-end m-lg-0"> 
+                                    <h5 className='mb-0'>{cuenta.description==('Caja de Ahorro en U$D'||'Cuenta corriente en U$D') ? 'U$D ' : '$ '}{cuenta.balance.toFixed(2)}</h5>
                                 </div>
                             </div>
                         </a>
--- src/views/dashboard.jsx
+++ src/views/dashboard.jsx
@@ -4,12 +4,17 @@ import Nav from "../components/Nav";
 import MovementList from "./MovementList";
 import { Cuentas } from "../components/Cuentas";
 import { NuevaCuenta } from "../components/NuevaCuenta";
+import { use } from "react";
 
 function Dashboard() {
   const [user, setUser] = useState(null);
   const navigate = useNavigate();
   const { idUser } = useParams(); // Obtener el userId de la URL
   const [idAccounts,setIdAccounts] = useState([]);
+  const [refresh,setRefresh] = useState(false);
+  let handleRefresh = ()=>{
+    setRefresh(!refresh);
+  };
 
   useEffect(() => {
     const storedUser = localStorage.getItem("user");
@@ -20,6 +25,10 @@ function Dashboard() {
     }
   }, [navigate]);
 
+  useEffect(()=>{
+    console.log(idAccounts);
+  },[idAccounts]);
+
   return (
     <> 
       <Nav navigate={navigate}/>
@@ -29,12 +38,11 @@ function Dashboard() {
             <div className="mb-3">
               <div className="d-flex mb-2 align-items-center">
                 <h3 className="mb-3">Cuentas</h3> 
-                <NuevaCuenta/>
+                {idAccounts.length < 5 && <NuevaCuenta idUser={idUser} handleRefresh={handleRefresh}/>}
               </div>
-              <div className="container-fluid mx-5 text-light">
-                <Cuentas idUser={idUser} setIdAccounts={setIdAccounts}/>
+              <div className="container-fluid mx-3 text-light">
+                <Cuentas idUser={idUser} setIdAccounts={setIdAccounts} refresh={refresh}/>
                 <MovementList idUser={idUser} />
-                {console.log(idAccounts)}
               </div>
             </div>
           </div>) : (
